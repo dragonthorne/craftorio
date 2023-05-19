69958d73182a12ec4b02d2862432377fae1e0ee7
Thorne <125685967+dragonthorne@users.noreply.github.com>
2023-05-19 14:41:13 -0500

    editing the defaults from the template

--- src/main/java/dragonthorne/craftorio/Craftorio/Craftorio.java
+++ src/main/java/dragonthorne/craftorio/Craftorio/Craftorio.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package dragonthorne.craftorio.craftorio;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.world.level.block.Block;
@@ -18,8 +18,8 @@ import org.slf4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
+@Mod("craftorio")
+public class Craftorio
 {
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
@@ -47,7 +47,7 @@ public class ExampleMod
     private void enqueueIMC(final InterModEnqueueEvent event)
     {
         // Some example code to dispatch IMC to another mod
-        InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
+        InterModComms.sendTo("craftorio", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
     }
 
     private void processIMC(final InterModProcessEvent event)
